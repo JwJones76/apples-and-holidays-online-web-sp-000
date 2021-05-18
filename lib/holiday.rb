a3aa350075aa78498e8e5ad1cbe978b86312c09e
Jacob W Jones <jacobwadejones76@gmail.com>
2021-05-18 19:44:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,48 +41,6 @@ def all_holidays_with_bbq(holiday_hash)
     end
   end.flatten.compact
 end
- 
-
- 
-
-
-def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].values.to_s
-
-end
-
-def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-  holiday_hash.each do |key, key2, value|
-    puts "#{key}:
-            #{key2}: #{value}"
-          
-        
-      
-    
-  end
-end
-
-def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-  bbq = []
-  holiday_hash.collect do |k, v|
-  v.collect do |k2, v2|
-    if  v2.include?("BBQ")
-      bbq << v2
-      bbq
-      end
-    end
-  end
-end
   
   
 
